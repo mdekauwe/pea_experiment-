17e474e16c99a711ebdecd62a1e137e6f9fb05d7
mdekauwe <mdekauwe@gmail.com>
2026-01-28 22:05:40 +0000

    code cleanup

--- scripts/run_and_analyse_by_week.R
+++ scripts/run_and_analyse_by_week.R
@@ -1,6 +1,3 @@
-# Fit each week indvidually
-
-
 library(lme4)
 library(lmerTest)
 library(simr)
@@ -12,15 +9,13 @@ library(emmeans)
 
 rm(list = ls())
 
-setwd("/Users/xj21307/Desktop/peas")  
+setwd("/Users/xj21307/src/R/pea_experiment")  
 source("R/simulate_experiment.R")
-
-
 
 ################################################################################
 
 #
-## Set your experiment parameters
+## Experiment parameters
 #
 
 params <- list(
@@ -30,15 +25,15 @@ params <- list(
   n_measured = 6,       # number of measured plants
   n_weeks  = 8,         # number of weeks of the experiment
   n_stress_weeks = 2,   # number of weeks with stress
-
+  
   mu = 10,              # baseline photosynthetic rate, umol m-2 s-1
-
+  
   effect_frac = list(
     drought = 0.15,     # 15% reduction
     heat = 0.20,        # 20% reduction
     drought_heat = 0.25 # 25% reduction
   ),
-
+  
   sd = list(
     run = 0.8,          # between run variability, umol m-2 s-1
     chamber  = 0.5,     # between chamber variability, umol m-2 s-1
@@ -53,51 +48,52 @@ out_dir <- "~/Desktop/"
 
 ################################################################################
 
+s <- simulate_experiment(params = params, treatments = treatments, seed = 124, 
+                         write_grid = TRUE, gradual_stress = TRUE, 
+                         out_dir = out_dir)
 
-s <- simulate_experiment(
-  params = params,
-  treatments = treatments,
-  seed = 123,
-  write_grid = TRUE,
-  gradual_stress = TRUE,
-  out_dir = out_dir
-)
-
-design <- s$design
-plants <- s$plants_full
+plant_grid <- s$plant_grid        # full plant layout (all plants)
+experiment_df <- s$experiment_df  # measured plants across weeks
 
 #############################################
 ## Fit split-plot mixed model
 #############################################
 
-design <- design %>%
-  mutate(week_f = factor(week))  # new factor variable
+# Week effects are interpreted as deviations relative to the final pre-stress 
+# week
+experiment_df <- experiment_df %>%
+  mutate(
+    week = factor(week, levels = 1:params$n_weeks),
+    week = relevel(week, ref = as.character(params$n_weeks - params$n_stress_weeks))
+  )
 
-# capture hierarchical structure of the split-plot experiment
-m_split_week <- lmer(Anet ~ treat * week_f +
-                  (1 | run/chamber) + # whole-plot variability for heat, nested
-                  (1 | run:chamber:plant_id), # sub-plot variability for drought
-                  data = design)
+# estimate week-specific treatment deviations relative to the reference week
+# tray position is fixed per pnat across weeks and treated as a random effect
+m_split_week <- lmer(Anet ~ drought * temp * week + 
+                       (1 | run/chamber) +            # chamber (whole-plot) random effect
+                       (1 | run:chamber:tray_pos) +   # tray-level random effect
+                       (1 | run:chamber:plant_id),    # plant-level random effect
+                     data = experiment_df)
+# plant-specific temporal trajectories
+# (1 + week | run:chamber:plant_id)
 
+fixed_eff <- tidy(m_split_week, effects = "fixed")
 
-tidy(m_split_week, effects = "fixed") %>%
-  filter(grepl("week_f", term)) %>%
+# Treatment × week effects
+fixed_eff %>%
+  filter(grepl("^week", term)) %>%
   arrange(term) %>%
-  print(n = 28)
+  print(n = Inf)
 
-# Analysis of Anet using a split-plot linear mixed model, treating week as a 
-# factor, showed that drought significantly reduced Anet starting in week 8 
-# (drought × week 8: estimate = -2.00, t = -3.94), while heat significantly 
-# reduced Anet in weeks 7 and 8 (heat × week 7: estimate = -1.25, t = -2.46; 
-# heat × week 8: estimate = -2.75, t = -5.43). The combination of drought and 
-# heat also showed significant effects in weeks 7 and 8 
-# (drought × heat × week 7: estimate = -1.06, t = -2.10; 
-# drought × heat × week 8: estimate = -2.44, t = -4.82), \
-# indicating that the joint impact of drought and heat on Anet was 
-# strongest in the later weeks of the experiment.
+# Three-way interaction
+fixed_eff %>%
+  filter(grepl("drought:temp:week", term)) %>%
+  arrange(term) %>%
+  print(n = Inf)
 
 
-em <- emmeans(m_split_week, ~ treat | week_f)
+em <- emmeans(m_split_week, ~ drought * temp | week)
+
 pdf("~/Desktop/emmeans_plot.pdf", width = 8, height = 6)
 plot(em)
 dev.off()
